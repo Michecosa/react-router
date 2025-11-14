64262afacb8486176bb42f6021e2d04ac9622878
Michela <micheladellagatta1@gmail.com>
2025-11-14 12:42:03 +0100

    Fix image layout

--- src/components/Prodotto.jsx
+++ src/components/Prodotto.jsx
@@ -4,14 +4,19 @@ export default function Prodotti({ products }) {
       {products.map((product) => (
         <div key={product.id} className="col-12 col-md-6 col-lg-4 col-xl-3">
           <div className="card h-100">
-            <div style={{ overflow: "hidden" }}>
+            <div style={{ width: "100%", height: "200px", padding: "1.1rem" }}>
               <img
                 src={product.image}
-                className="card-img-top"
                 alt={product.title}
-                style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "contain",
+                  objectPosition: "center",
+                }}
               />
             </div>
+
             <div className="card-body">
               <h5 className="card-title">{product.title}</h5>
               <p className="card-text">
