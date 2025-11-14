3673b431db9f895743bc74a5a1debe9d473bb67a
Michela <micheladellagatta1@gmail.com>
2025-11-14 11:50:45 +0100

    Implement routing structure with DefaultLayout and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-function App() {
-  return <></>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Home from "./pages/Homepage";
+import Prodotti from "./pages/Prodotti";
+import ChiSiamo from "./pages/ChiSiamo";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/prodotti" element={<Prodotti />} />
+          <Route path="/chisiamo" element={<ChiSiamo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
