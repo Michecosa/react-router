afa9aa039262c6056cf7ce18a7a1ba4184891df4
Michela <micheladellagatta1@gmail.com>
2025-11-14 13:20:12 +0100

    Add Homepage welcome message

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,10 @@
 export default function Homepage() {
-  return <></>;
+  return (
+    <div>
+      <div className="container mb-5">
+        <h1 className="display-4 fw-bold">Benvenuto!</h1>
+        <p className="lead">Sei nella homepage</p>
+      </div>
+    </div>
+  );
 }
