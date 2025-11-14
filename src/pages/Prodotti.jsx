da4d4cb9048a7df68064b683b84a68ec39c696ec
Michela <micheladellagatta1@gmail.com>
2025-11-14 18:22:17 +0100

    Implement product filtering logic by category

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -5,6 +5,22 @@ import CardProdotti from "../components/CardProdotti";
 export default function Prodotti({ urlProducts }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selected, setSelected] = useState(0);
+  const [filterProducts, setFilterProducts] = useState(products);
+
+  const filtroCategoria = (index) => {
+    if (categories[index] === "All") {
+      setFilterProducts(products);
+    } else {
+      setFilterProducts(
+        products.filter((el) => el.category === categories[index])
+      );
+    }
+  };
+
+  useEffect(() => {
+    filtroCategoria(selected);
+  }, [products, selected]);
 
   useEffect(() => {
     axios
@@ -16,6 +32,13 @@ export default function Prodotti({ urlProducts }) {
       .catch((err) => console.log(err));
   }, [urlProducts]);
 
+  const categories = [];
+  products.forEach((product) => {
+    if (!categories.includes(product.category))
+      categories.push(product.category);
+  });
+  categories.unshift("All");
+
   return (
     <div className="container my-4">
       {loading ? (
@@ -23,7 +46,21 @@ export default function Prodotti({ urlProducts }) {
       ) : (
         <div>
           <h2 className="mb-4">I nostri prodotti</h2>
-          <CardProdotti products={products} />
+          {categories.map((category, index) => {
+            return (
+              <button
+                key={index}
+                className={`btn btn-secondary me-2 mb-5 ${
+                  index === selected && "btn-info"
+                }`}
+                onClick={() => setSelected(index)}
+              >
+                {category}
+              </button>
+            );
+          })}
+
+          <CardProdotti products={filterProducts} />
         </div>
       )}
     </div>
