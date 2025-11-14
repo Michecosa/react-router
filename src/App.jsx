acc10b390060922e0043430e9975afc21dd00df1
Michela <micheladellagatta1@gmail.com>
2025-11-14 12:14:29 +0100

    Spent 30 minutes debugging… turns out I wasn’t even on the products page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import Home from "./pages/Homepage";
 import Prodotti from "./pages/Prodotti";
 import ChiSiamo from "./pages/ChiSiamo";
 
+const endpoint = "https://fakestoreapi.com/products";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
           <Route index element={<Home />} />
-          <Route path="/prodotti" element={<Prodotti />} />
+          <Route
+            path="/prodotti"
+            element={<Prodotti urlProducts={endpoint} />}
+          />
           <Route path="/chisiamo" element={<ChiSiamo />} />
         </Route>
       </Routes>
--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,3 +1,20 @@
-export default function Prodotti() {
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function Prodotti({ urlProducts }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(urlProducts)
+      .then((response) => {
+        console.log(response.data);
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [urlProducts]);
+
   return <></>;
 }
