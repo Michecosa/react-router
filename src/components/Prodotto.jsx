4c4647f83c8df806ee264bb21c93053cb16185e8
Michela <micheladellagatta1@gmail.com>
2025-11-14 12:38:01 +0100

    Refactor product component layout for improved display and responsiveness

--- src/components/Prodotto.jsx
+++ src/components/Prodotto.jsx
@@ -1,19 +1,24 @@
-export default function Prodotto({ products }) {
+export default function Prodotti({ products }) {
   return (
-    <div className="card" style={{ width: "18rem" }}>
+    <div className="row g-4">
       {products.map((product) => (
-        <div key={product.id}>
-          <img
-            src={product.image}
-            className="card-img-top"
-            alt={product.title}
-          />
-          <div className="card-body">
-            <h3 className="card-text">{product.title}</h3>
-            <p className="card-text">
-              {product.category} - &euro;{product.price}
-            </p>
-            <p className="card-text">{product.description}</p>
+        <div key={product.id} className="col-12 col-md-6 col-lg-4 col-xl-3">
+          <div className="card h-100">
+            <div style={{ overflow: "hidden" }}>
+              <img
+                src={product.image}
+                className="card-img-top"
+                alt={product.title}
+                style={{ objectFit: "cover", width: "100%", height: "100%" }}
+              />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">{product.title}</h5>
+              <p className="card-text">
+                {product.category} - &euro;{product.price}
+              </p>
+              <p className="card-text">{product.description}</p>
+            </div>
           </div>
         </div>
       ))}
