5e0188bbb74a1d90c9df61bab4a3751a374aeddd
Michela <micheladellagatta1@gmail.com>
2025-11-14 20:53:38 +0100

    Pass cart prop to Header in DefaultLayout component

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function DefaultLayout() {
+export default function DefaultLayout({ cart }) {
   return (
     <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-      <Header />
+      <Header cart={cart} />
 
       <main className="flex-grow-1 pb-2">
         <Outlet />
