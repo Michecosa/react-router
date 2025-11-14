7526628db73ac2c8b75a37dfaa675fbcb9b4542e
Michela <micheladellagatta1@gmail.com>
2025-11-14 12:31:18 +0100

    Create card structure with all product infos

--- src/components/Prodotto.jsx
+++ src/components/Prodotto.jsx
@@ -1,12 +1,22 @@
 export default function Prodotto({ products }) {
   return (
-    <>
+    <div className="card" style={{ width: "18rem" }}>
       {products.map((product) => (
         <div key={product.id}>
-          <h3>{product.name}</h3>
-          <p>{product.description}</p>
+          <img
+            src={product.image}
+            className="card-img-top"
+            alt={product.title}
+          />
+          <div className="card-body">
+            <h3 className="card-text">{product.title}</h3>
+            <p className="card-text">
+              {product.category} - &euro;{product.price}
+            </p>
+            <p className="card-text">{product.description}</p>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   );
 }
