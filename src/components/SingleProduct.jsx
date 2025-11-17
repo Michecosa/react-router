762cb6a1b07a203fbda64a0ab2bc2a55e5b1d693
Michela <micheladellagatta1@gmail.com>
2025-11-17 12:34:48 +0100

    Add style to the SingleProduct page

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -24,16 +24,50 @@ export default function SingleProduct({ urlProducts }) {
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div className="row g-4">
-          <div>
-            <img src={product.image} alt={product.title} />
+        <div className="row g-5 align-items-start">
+          <div className="col-md-5 d-flex justify-content-center">
+            <div className="p-3" style={{ maxWidth: "350px" }}>
+              <img
+                src={product.image}
+                alt={product.title}
+                className="img-fluid"
+                style={{ objectFit: "contain" }}
+              />
+            </div>
           </div>
-          <div>
-            <h2>{product.title}</h2>
-            <p>{product.category}</p>
-            <p>{product.rating.rate}</p>
-            <p>{product.price.toFixed(2)}</p>
-            <p>{product.description}</p>
+
+          <div className="col-md-7">
+            <h2 className="fw-bold mb-1">{product.title}</h2>
+            <div className="d-flex justify-content-between">
+              <p className="text-muted" style={{ fontSize: "0.75rem" }}>
+                {product.category}
+              </p>
+              <p
+                className="text-body-tertiary mb-2"
+                style={{ fontSize: "0.85rem" }}
+              >
+                <span
+                  className="text-warning fw-bold mb-1 text-end me-2"
+                  style={{ fontSize: "0.85rem" }}
+                >
+                  <i className="bi bi-star-fill"></i> {product.rating.rate}
+                </span>
+                ({product.rating.count})
+              </p>
+            </div>
+            <h4 className="text-success fw-bold mb-3">
+              €{product.price.toFixed(2)}
+            </h4>
+            <div style={{ maxWidth: "600px" }}>
+              <p className="fw-semibold mt-4 mb-0">Product details:</p>
+              <p>{product.description}</p>
+            </div>
+            <button
+              className="btn btn-outline-primary mt-3"
+              onClick={() => navigate(-1)}
+            >
+              Torna indietro
+            </button>
           </div>
         </div>
       )}
